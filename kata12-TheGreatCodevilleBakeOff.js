1a403bd32fbbfdcd8765831c44bfbfa7356ae7ca
Max Li <hope_max@outlook.com>
2021-07-15 13:20:01 -0400

    Kata12

---
+++ kata12-TheGreatCodevilleBakeOff.js
@@ -0,0 +1,47 @@
+const chooseRecipe = function(bakeryA, bakeryB, recipes) {
+  let index;
+  for (let i = 0; i < recipes.length; i++) {
+    if (bakeryA.indexOf(recipes[i]['ingredients'][0]) + bakeryB.indexOf(recipes[i]['ingredients'][0]) !== -2 
+        && bakeryA.indexOf(recipes[i]['ingredients'][1]) + bakeryB.indexOf(recipes[i]['ingredients'][1]) !== -2) {
+      return recipes[i]['name'];
+    }
+  }
+}
+
+let bakeryA = ['saffron', 'eggs', 'tomato paste', 'coconut', 'custard'];
+let bakeryB = ['milk', 'butter', 'cream cheese'];
+let recipes = [
+    {
+        name: 'Coconut Sponge Cake',
+        ingredients: ['coconut', 'cake base']
+    },
+    {
+        name: 'Persian Cheesecake',
+        ingredients: ['saffron', 'cream cheese']
+    },
+    {
+        name: 'Custard Surprise',
+        ingredients: ['custard', 'ground beef']
+    }
+];
+
+console.log(chooseRecipe(bakeryA, bakeryB, recipes));
+
+bakeryA = ['potatoes', 'bay leaf', 'raisins'];
+bakeryB = ['red bean', 'dijon mustard', 'apples'];
+recipes = [
+    {
+        name: 'Potato Ganache',
+        ingredients: ['potatoes', 'chocolate']
+    },
+    {
+        name: 'Sweet Fish',
+        ingredients: ['anchovies', 'honey']
+    },
+    {
+        name: "Nima's Famous Dijon Raisins",
+        ingredients: ['dijon mustard', 'raisins']
+    }
+];
+
+console.log(chooseRecipe(bakeryA, bakeryB, recipes));
